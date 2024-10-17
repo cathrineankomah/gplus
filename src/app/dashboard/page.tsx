e1a5ea0e41b1e63ba5b4f0d5afd7bd9fdabf9c6c
catherine <gainplus23@gmail.com>
2024-10-17 03:03:00 +0000

    added edge for build

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 import { getUsers } from "@/server/api/query/user";
 import React from "react";
 
-// export const runtime = "edge";
+export const runtime = "edge";
 export default async function DashboardPage() {
   const users = await getUsers();
-  console.log(users);
-  return <div>Dashboard</div>;
+
+  return <div>{JSON.stringify(users)}</div>;
 }
