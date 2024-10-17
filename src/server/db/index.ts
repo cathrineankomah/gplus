076cc6feb01c5f3a129673df8a2fd82dfaba289a
catherine <gainplus23@gmail.com>
2024-10-17 03:19:57 +0000

    ddd

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-import * as schema from "./schema";
+import { createClient } from "@libsql/client/web";
 
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
-export const db = drizzle(client, { schema });
+export const db = drizzle(client);
